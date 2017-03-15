d0884b4fccc4960b5281d04b97d72f7744b88e12
Sameer Al-Sakran <sameer@expa.com>
2017-03-14 19:26:40 -0700

    rails wip

--- rails/embedded_analytics/config/routes.rb
+++ rails/embedded_analytics/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   get 'stats/index'
+  get 'stats/signed_chart'
+  get 'stats/signed_dashboard'
+  get 'stats/signed_public_dashboard'
   root 'welcome#index'
 end
